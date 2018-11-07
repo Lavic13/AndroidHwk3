d9bac1e1dc04e2d7994bfca1142fd5696b32884c
Lavic13 <vmoo1392@gmail.com>
2018-11-07 15:38:31 -0800

    started json parsing

--- app/src/main/java/com/example/rkjc/news_app_2/JsonUtils.java
+++ app/src/main/java/com/example/rkjc/news_app_2/JsonUtils.java
@@ -5,9 +5,13 @@ import android.util.Log;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.util.ArrayList;
+import com.example.rkjc.news_app_2.NewsItem;
 
 public class JsonUtils {
+    public static ArrayList<NewsItem> parseNews(String jsonResult){
+        ArrayList<NewsItem> newsItemList = new ArrayList<>();
 
+    }
 
 }
 
