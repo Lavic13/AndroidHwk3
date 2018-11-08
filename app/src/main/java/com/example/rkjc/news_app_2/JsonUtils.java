1e16d1d4cf449d8ca4d90aaa3329335e8f1904a2
Lavic13 <vmoo1392@gmail.com>
2018-11-07 16:14:20 -0800

    completed json parsing

--- app/src/main/java/com/example/rkjc/news_app_2/JsonUtils.java
+++ app/src/main/java/com/example/rkjc/news_app_2/JsonUtils.java
@@ -3,6 +3,7 @@ package com.example.rkjc.news_app_2;
 import android.util.Log;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.ArrayList;
 import com.example.rkjc.news_app_2.NewsItem;
@@ -10,6 +11,26 @@ import com.example.rkjc.news_app_2.NewsItem;
 public class JsonUtils {
     public static ArrayList<NewsItem> parseNews(String jsonResult){
         ArrayList<NewsItem> newsItemList = new ArrayList<>();
+        try{
+            JSONObject mainJSONObject = new JSONObject(jsonResult);
+            JSONArray items = mainJSONObject.getJSONArray("articles");
+
+            for(int i = 0; i < items.length(); i++){
+                JSONObject item = items.getJSONObject(i);
+                NewsItem curr_news = new NewsItem();
+                
+                curr_news.setAuthor(item.getString("author"));
+                curr_news.setTitle(item.getString("title"));
+                curr_news.setDescription(item.getString("description"));
+                curr_news.setUrl(item.getString("url"));
+                curr_news.setUrl(item.getString("urlToImage"));
+                curr_news.setPublishedAt(item.getString("publishedAt"));
+            }
+
+        }catch(JSONException e) {
+            e.printStackTrace();
+        }
+        return newsItemList;
 
     }
 
