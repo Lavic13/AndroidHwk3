37f6d9982d2f216387585e603c2911e9f077d7b6
Lavic13 <vmoo1392@gmail.com>
2018-11-17 22:03:18 -0800

    database complete

---
+++ app/src/main/java/com/example/rkjc/news_app_2/Database/NewsItemDatabase.java
@@ -0,0 +1,22 @@
+package com.example.rkjc.news_app_2.Database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {NewsItem.class}, version = 1)
+public abstract class NewsItemDatabase extends RoomDatabase {
+    private static NewsItemDatabase instance;
+
+    public abstract NewsItemDao newsItemDao();
+
+    public  static  synchronized  NewsItemDatabase getInstance(Context context){
+        if(instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(), NewsItemDatabase.class, "newsItem_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
+}
