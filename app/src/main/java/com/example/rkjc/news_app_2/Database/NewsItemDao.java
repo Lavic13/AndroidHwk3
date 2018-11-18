2875e81521766caeb81ee2bf6433b5f031380206
Lavic13 <vmoo1392@gmail.com>
2018-11-17 19:10:11 -0800

    completed part 3, still need database though

---
+++ app/src/main/java/com/example/rkjc/news_app_2/Database/NewsItemDao.java
@@ -0,0 +1,26 @@
+package com.example.rkjc.news_app_2.Database;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface NewsItemDao {
+
+    @Query("SELECT * FROM news_item")
+    LiveData<List<NewsItem>> loadAlltasks();
+
+    @Query("DELETE FROM news_item")
+    void clearAll();
+
+    @Insert
+    void insert(List<NewsItem> items);
+
+
+}
